360c50835383ce2ab6509871c92f0015b2d7eac6
benedictssemuju <91035099+benedictssemuju@users.noreply.github.com>
2021-10-07 15:10:57 -0500

    Update test.js

--- w2d4inheritance/test.js
+++ w2d4inheritance/test.js
@@ -7,14 +7,14 @@ describe("String Filter", () => {
 });
 describe("Array BubbleSort", () => {
     it("Bubble sort is an extension on the Array prototype that sorts any array with the bubble sort algorithm", () => {
-        expect([-2, 0, 1, 3, 4, 6]).to.eql([6, 4, 0, 3, -2, 1].bubbleSort());
+        assert.deepEqual([-2, 0, 1, 3, 4, 6]),([6, 4, 0, 3, -2, 1].bubbleSort());
     });
 });
 describe("Inheritance", () => {
     it("Teacher object derived from the Person class", () => {
         const me = new Teacher();
-        me.initialize("Albert Einstein", 58);
+        me.initialize("Anna Bella", 26);
         expect(me instanceof Person).to.be.true;
-        expect("Albert Einstein, 58 years old.").to.eql(me.describe());
+        expect("Anna Bella, 26 years old.").to.eql(me.describe());
     });
 });
