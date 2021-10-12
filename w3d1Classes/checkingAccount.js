af8a9ee99ab8759c1cf6676589eccfaefdfb86eb
benedictssemuju <91035099+benedictssemuju@users.noreply.github.com>
2021-10-11 21:46:00 -0500

    Update checkingAccount.js

--- w3d1Classes/checkingAccount.js
+++ w3d1Classes/checkingAccount.js
@@ -25,8 +25,5 @@ class CheckingAccount extends Account{
         return this._balance < 0 ? `Warning, low balance CheckingAccount 3: balance: -100 overdraft limit: 500`
             : "No action for Checking account";
     }
-    endOfMonth() {
-        return this._accounts.map(a => a.endOfMonth()).reduce((a, b) => a + "\n" + b, "");
-    }
 
 }
