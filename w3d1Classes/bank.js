024c7f3b4e7745c93399d04686160f8f26d0e3a8
benedictssemuju <91035099+benedictssemuju@users.noreply.github.com>
2021-10-12 10:39:24 -0500

    Create bank.js

---
+++ w3d1Classes/bank.js
@@ -0,0 +1,25 @@
+class Bank {
+    static nextNumber = 123456;
+
+    constructor() {
+        this._accounts = [];
+    }
+    getAccount(){
+        return this._accounts;
+    }
+    addAccount(){
+        this._accounts.push(new Acccount(Bank.nextNumber++));
+    }
+    addSavingsAccount(interest){
+        this._accounts.push(new SavingsAccount(Bank.nextNumber++,interest));
+    }
+    addCheckingAccount(overdraft) {
+        this._accounts.push(new CheckingAccount(Bank.nextNumber++, overdraft));
+    }
+    closeAccount(number){
+        this._accounts = this._accounts.filter(x=>x.getNumber()!==number)
+    }
+    accountReport(){
+        this._accounts.forEach(x=>console.log(x.endOfMonth()));
+    }
+}
