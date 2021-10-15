d207e763bd0f77105c30444dfef6597ce553c17e
benedictssemuju <91035099+benedictssemuju@users.noreply.github.com>
2021-10-14 19:49:05 -0500

    Create app.js

---
+++ w3d4Node/app.js
@@ -0,0 +1,34 @@
+"use strict"
+const readline = require('readline').createInterface({
+ input: process.stdin,
+ output: process.stdout,
+});
+
+//Number 1
+readline.question('What is your name? ', name => {
+ console.log(`Welcome ${name}`);
+readline.question('What is your age? ', age => {
+    if(age<16){
+        console.log("You’re not allowed to drive in Iowa");
+    }else{ console.log("You’re allowed to get a drivers license in Iowa");}
+    readline.close();
+});
+});
+
+//Number 2
+
+function sumOfNums(){
+    let sum =0;
+    const getNumber = function(){
+        readline.question('What is your number or "stop" to exit? ', response => {
+            if(response.toLowerCase==="stop"){
+                console.log(`Sum: ${sum}`);
+            }else{
+                sum+=response;
+                getNumber();
+            }
+            
+           });
+    } 
+    getNumber();
+};
