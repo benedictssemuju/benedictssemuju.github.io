c906e0826436916174fe32408aea6533b9c4ae8d
benedictssemuju <91035099+benedictssemuju@users.noreply.github.com>
2021-10-15 21:33:20 -0500

    Update index.js

--- w3d5Express/q2/index.js
+++ w3d5Express/q2/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.urlencoded({extended: false}));
-app.set('port', process.env.PORT || 3200);
+app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
     res.send('<form action="/result" method="POST"> ' +
